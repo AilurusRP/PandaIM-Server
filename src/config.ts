9a6ac1a8e7652f606430ea210e1ce7bf02e9cef9
AilurusRedPanda <1148648777@qq.com>
2022-02-22 17:06:45 +0800

    deleted config.ts from the ropo

--- src/config.ts
+++
@@ -1,6 +0,0 @@
-export default {
-    dbAddr: "mongodb://127.0.0.1:27017/pandaTest",
-    root: "C:\\Users\\11486\\Desktop\\workdesk\\node\\PandaIM-Server\\",
-    port: 3080,
-    secret: "Your secret key to encrypt the token",
-};
